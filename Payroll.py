864f1abc408a55f88e7a93b5866653ffc086e606
johnreznicek <john.reznicek@du.edu>
2024-05-06 17:56:47 -0600

    total added

--- Payroll.py
+++ Payroll.py
@@ -1,13 +1,19 @@
 from Employee import Employee
 
 class Payroll():
-    def __init__(self):
-        self.emp = []
+  def __init__(self):
+    self.emp = []
 
-    def __repr__(self):
-        return str(self.emp)
-    
-    def addEmp(self, e):
-        self.emp.append(e)
+  def __repr__(self):
+    return str(self.emp)
+  
+  def addEmp(self, e):
+    self.emp.append(e)
 
-    
+  def total(self):
+    """Calculates and returns the total payroll for all employees."""
+    total_pay = 0
+    for employee in self.emp:
+      # Assuming Employee class has a method to get employee pay (e.get_pay())
+      total_pay += employee.get_pay()
+    return total_pay
