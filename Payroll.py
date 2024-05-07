704d87c8352253a4477527acb8b478260d40a91c
johnreznicek <john.reznicek@du.edu>
2024-05-06 18:07:53 -0600

    sortAdded

--- Payroll.py
+++ Payroll.py
@@ -11,9 +11,17 @@ class Payroll():
     self.emp.append(e)
 
   def total(self):
-    """Calculates and returns the total payroll for all employees."""
     total_pay = 0
     for employee in self.emp:
       # Assuming Employee class has a method to get employee pay (e.get_pay())
       total_pay += employee.get_pay()
     return total_pay
+  def sort(self):
+    for i in range(len(self.emp)):
+      # Find the index of the employee with the maximum pay in the unsorted part
+      max_index = i
+      for j in range(i + 1, len(self.emp)):
+        if self.emp[j].get_pay() > self.emp[max_index].get_pay():
+          max_index = j
+      # Swap the employee with the maximum pay with the current element
+      self.emp[i], self.emp[max_index] = self.emp[max_index], self.emp[i]
